42401cb1f84c01e805c9e9b9b9e6695d9a715fae
Valentinos Galanos <sonidelav@hotmail.com>
2021-06-07 17:14:21 +0300

    error page

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="container">
-        <h1 v-if="error.statusCode === 404">Page not found</h1>
-        <h1 v-else>An error occurred</h1>
-        <NuxtLink to="/">Home page</NuxtLink>
+        <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+            <div class="mb-3">
+                <h1 v-if="error.statusCode === 404">Oops, there is nothing here!</h1>
+                <h1 v-else>Something went wrong</h1>
+            </div>
+            <b-btn variant="primary" to="/" class="p-3">Take me home, I've been lost</b-btn>
+        </div>
     </div>
 </template>
 
